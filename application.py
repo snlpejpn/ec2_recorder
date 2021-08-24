a45a3a9a8519108fe07a11001eaa97fc6be77a36
Suzuki Naoaki <naoaki-s@ahcclcsa01.naist.jp>
2021-08-24 17:32:58 +0900

    add index

--- application.py
+++ application.py
@@ -7,18 +7,18 @@ import os
 application = Flask(__name__)
 
 
-# @application.route("/", methods=['POST', 'GET'])
-# def index():
-#     if request.method == "POST":
-#         f = open('./file.wav', 'wb')
-#         f.write(request.files["audio_data"].read())
-#         f.close()
-#         return "task End"
-#         # return render_template('index.html')
-#         # return render_template('index.html', method="POST") *original
-#     else:
-#         return render_template('index.html')
+@application.route("/", methods=['POST', 'GET'])
+def index():
+    if request.method == "POST":
+        f = open('./file.wav', 'wb')
+        f.write(request.files["audio_data"].read())
+        f.close()
+        return "task End"
+        # return render_template('index.html')
+        # return render_template('index.html', method="POST") *original
+    else:
+        return render_template('index.html')
 
-@application.route('/')
-def test():
-    return "This is the test page."
+# @application.route('/')
+# def test():
+#     return "This is the test page."
